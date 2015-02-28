abe3207ee938b60697e7d72847e6dcb36216f888
heiydvl <heidy.villarreal@spsmail.cuny.edu>
2015-02-28 18:25:29 +0000

    task_01

--- task_01.py
+++ task_01.py
@@ -4,6 +4,19 @@
 
 
 def know_what_i_mean(wink, numwink=2):
+    """This function determines what you mean.
+
+    Args:
+        wink (int): No default value.
+        numwink (int): Default value 2.
+
+    Returns:
+        str: What I mean.
+
+    Examples:
+
+        >>>
+    """
     winks = (wink * numwink).strip()
     nudges = ('nudge ' * numwink).strip()
     retstr = 'Know what I mean? {}, {}'.format(winks, nudges)
